1f57d8700b55f2024a8fb838277faa7558143b22
SCWinter259 <catungthongkhi2002@gail.com>
2023-07-17 22:39:21 -0600

    done movie query function, fixed bugs, tested

--- Controllers/QueryFunctions.py
+++ Controllers/QueryFunctions.py
@@ -1,5 +1,6 @@
 from typing import Union
 import config
+from Controllers.HelperFunctions import _chop, _search_title, _search_member_name, _search_member_role
 from Models.Customer import Customer
 from Models.Editor import Editor
 from Models.Session import Session
@@ -183,8 +184,10 @@ def search(text: str) -> list[Movie]:
                 all_mid.append(mid)
                 all_count.append(count)
 
-    mid_list = zip(all_mid, all_count)
-    mid_list.sort(key=lambda index: index[1], reversed=True)        # sort the zipped list by count, descending order
+    mid_list = list(zip(all_mid, all_count))
+    mid_list.sort(key=lambda index: index[1], reverse=True)        # sort the zipped list by count, descending order
+
+    # print(mid_list)
 
     movie_list = []
     for item in mid_list:
@@ -204,100 +207,6 @@ def search(text: str) -> list[Movie]:
         movie_list.append(movie)
 
     return movie_list
-
-# support functions
-def _chop(text: str) -> list[str]:
-    '''
-    Takes in a string, return the list of words in that strings,
-    after getting rid of all white spaces, comma, colon, semi-colon, and question marks
-    '''
-    text = text.replace(',', ' ')
-    text = text.replace(':', ' ')
-    text = text.replace(';', ' ')
-    text = text.replace('?', ' ')
-
-    return text.split(' ')
-
-def _search_title(word: str) -> list[str, int]:
-    '''
-    This function searches for movies with title containing
-    the given keyword.
-    Returns a list of [mid, number of repetitions]
-    '''
-    mid_list = []
-    config.cursor.execute(
-        '''SELECT mid, title FROM movies WHERE title LIKE :word COLLATE NOCASE''',
-        {"word": word}
-    )
-    results = config.cursor.fetchall()      # got a list of (mid, title)
-
-    # count the number of repetition of the keyword in the title
-    for (mid, title) in results:
-        title_list_split = _chop(title)
-        count = title_list_split.count(word)
-        mid_list.append([mid, count])       # append appropriate data into the list
-
-    return mid_list
-
-def _search_member_role(word: str) -> list[str, int]:
-    '''
-    This function searches for movies with a role containing
-    the given keyword.
-    Returns a list of [mid, number of repetitions]
-    '''
-    mid_list = []
-    config.cursor.execute(
-        '''SELECT mid, role FROM casts WHERE role LIKE :word COLLATE NOCASE''',
-        {"word": word}
-    )
-    results = config.cursor.fetchall()      # got a list of (mid, role)
-
-    # count the number of repetition of the keyword in the role
-    for (mid, role) in results:
-        title_list_split = _chop(role)
-        count = title_list_split.count(word)
-        mid_list.append([mid, count])       # append appropriate data into the list
-
-    return mid_list
-
-def _search_member_name(word: str) -> list[str, int]:
-    '''
-    This function searches for movies with a member whose
-    name contains the given keyword.
-    Returns a list of [mid, number of repetitions]
-    '''
-    mid_list = []
-    config.cursor.execute(
-        '''SELECT pid, name FROM moviePeople WHERE name LIKE :word COLLATE NOCASE''',
-        {"word": word}
-    )
-    results = config.cursor.fetchall()      # got a list of (pid, name)
-
-    # count the number of repetition of the keyword in the name
-    pid_list = []
-    for (pid, name) in results:
-        title_list_split = _chop(name)
-        count = title_list_split.count(word)
-        pid_list.append([pid, count])       # append appropriate data into the list
-
-    for [pid, count] in pid_list:           # for each pid, find the mid
-        config.cursor.execute(
-            '''SELECT mid FROM casts WHERE pid=:pid''',
-            {"pid": pid}
-        )
-        all_mid = config.cursor.fetchall()
-        for mid in all_mid:         # does the mid exist in mid_list yet?
-            found = False
-            for i in range(len(mid_list)):
-                if mid_list[i][0] == mid:       # if yes, increase count
-                    found = True
-                    mid_list[i][1] += count
-                    break
-            if not found:           # if not, append new sub list
-                mid_list.append([mid, count])
-
-    return mid_list
-
 
 # below this line are scrap functions
 
